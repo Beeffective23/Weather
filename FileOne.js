710049655c1ff28bee95ad6789b10bc79d7d804e
Peter <115339295+Beeffective23@users.noreply.github.com>
2023-01-02 21:36:04 +0200

    Delete FileOne.js

--- FileOne.js
+++
@@ -1,9 +0,0 @@
-let myObject = {
-  names: ["Peter", "Ivan", "Valeri"],
-};
-
-let myButton = document.querySelector("button");
-myButton.addEventListener("click", function () {
-  myObject.names.push("Vesela");
-});
-export { myObject};
